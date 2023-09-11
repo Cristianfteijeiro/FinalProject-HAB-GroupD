2787efed6921ea4bd7212e316e9436bbb7fcdcde
Xela <xelalonghi@gmail.com>
2023-09-11 21:25:28 +0200

    AñadidoHome

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,9 +4,6 @@ export const Home = () => {
   return (
     <section className="home">
       <h1>"VIAJAR ES LA ÚNICA COSA QUE COMPRAS QUE TE HACE MÁS RICO."</h1>
-      <h2>
-        Descubre lugares, pregunta, opina y sumérgete hacia nuevos horizontes
-      </h2>
     </section>
   );
 };

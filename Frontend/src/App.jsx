96629b3cecd3c058e73b5e68ba1ff17d98bc759f
Fran <fran.bana.vazquez@gmail.com>
2023-09-12 21:28:29 +0200

    Recomendaciones por id, Not Found, CSS

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,10 +5,12 @@ import { Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Post } from "./pages/Post";
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
 import { Register } from "./pages/Register";
 import { Buscador } from "./pages/Buscador";
 import { MisPosts } from "./pages/MisPosts";
+import { Recomendaciones } from "./pages/Recomendaciones";
+import { Footer } from "./components/Footer";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
         <Route path="/post" element={<Post />} />
         <Route path="/buscador" element={<Buscador />} />
         <Route path="/misposts" element={<MisPosts />} />
+        <Route path="/recomendaciones" element={<Recomendaciones />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </main>
---
+++ Frontend/src/components/Recomendacion.jsx
@@ -0,0 +1,23 @@
+import "../Styles/Recomendacion.css";
+
+export default function Recomendacion() {
+  return (
+    <section>
+      <div className="datos">
+        <h1>POST DE PAISANO ALEATORIO</h1>
+        <h2>TÍTULO</h2>
+        <p>Lugar</p>
+        <p>Entradilla</p>
+        <p>Texto</p>
+        <ul>
+          <li>*</li>
+          <li>*</li>
+          <li>*</li>
+        </ul>
+      </div>
+      <div>
+        <img src="../src/assets/images/playa.jpg" />"
+      </div>
+    </section>
+  );
+}

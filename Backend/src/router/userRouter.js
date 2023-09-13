41fb95b6c5f6be68abefcaf73546b7ceb51992fb
Dani Novo <daninovo33@gmail.com>
2023-09-13 20:57:59 +0200

    Backend retocado + Frontend .envexample

--- Backend/src/router/userRouter.js
+++ Backend/src/router/userRouter.js
@@ -6,7 +6,7 @@ const userLogged = require("../middlewares/userLogged");
 
 const { createUser, loginUser, avatarUser } = require("../controllers/users");
 
-router.post("/usuarios", createUser);
+router.post("/registro", createUser);
 router.post("/usuarios/login", loginUser);
 
 router.put("/usuarios/:idUser", userLogged, avatarUser);
